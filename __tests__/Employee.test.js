cc0efaf878aa41a2af40ca944d7914325581159e
MrTofuuu <84428685+MrTofuuu@users.noreply.github.com>
2021-10-07 23:41:25 -0500

    wrote inital tests for employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,67 @@
+const Employee = requrie('../lib/Employee');
+
+
+describe('Employee', () => {
+    describe('Initialization', () => {
+        it('should create an object with a name, id, and email if provided valid arguments', () => {
+            const employee = new Employee('Monkey D. Luffy', 420, 'monkey@luffy.com');
+
+            expect(employee.name).toEqual('Monkey D. Luffy');
+            expect(employee.id).toEqual(420);
+            expect(employee.email).toEqual('monkey@luffy.com');
+        }); // Initializtion of object check if valid arguments
+
+        it('should throw an error if provided no arguments', () => {
+            // Wrap the object initialization in a callback function that Jest will run 
+            const cb = () => new Employee();
+
+            //should throw an error in the callback function
+            expect(cb).toThrow();
+        }); //Negative check on callback function with empty arguments
+
+        it('should throw an error if only name is provided', () => {
+            // Wrap the object initialization in a callback function that Jest will run 
+            const cb = () => new Employee('Monkey D. Luffy');
+
+            // define the error message that is expected to be thrown
+            const err = new Error('The only parameter used is <name>. <id> and <email> parameters are required');
+
+            // Verify that the correct error was thrown when the callback is executed
+            expect(cb).toThrowError(err);
+        }); // Checking to see if only <name> is provided
+
+        it('should throw an error if only name and id is provided', () => {
+            // Wrap the object initialization in a callback function that Jest will run 
+            const cb = () => new Employee('Monkey D. Luffy', 420);
+
+            // define the error message that is expected to be thrown
+            const err = new Error('The only parameter used is <name> and <id>.<email> parameter is required');
+
+            // Verify that the correct error was thrown when the callback is executed
+            expect(cb).toThrowError(err);
+        }); // Checking to see if only <name> is provided
+
+        it('should throw an error if <name> is not a string', () => {
+            const cb = () => new Employee(5, 2, 'monkey@luffy.com');
+            const err = new Error('Expected paramter <name> to be a non-empty string');
+
+            expect(cb).toThrowError(err);
+        }); // Checking to see if <name> is not a string
+
+        it('should throw an error if <id> is not a number', () => {
+            const cb = () => new Employee('Monkey D. Luffy', '2', 'monkey@luffy.com');
+            const err = new Error('Expected paramter <id> to be a number');
+
+            expect(cb).toThrowError(err);
+        }); // Checking to see if <id> is not a number
+
+        it('should throw an error if <email> is not a string', () => {
+            const cb = () => new Employee('Monkey D. Luffy', 2, 1);
+            const err = new Error('Expected paramter <email> to be a non-empty string');
+
+            expect(cb).toThrowError(err);
+        }); // Checking to see if <email> is not a string
+
+
+    });
+});
