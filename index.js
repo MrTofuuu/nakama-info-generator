7ee4ce7f6b6149a27236fcf483a2542b3c5d031d
MrTofuuu <84428685+MrTofuuu@users.noreply.github.com>
2021-10-18 00:24:31 -0500

    adding more info to the index.js

--- index.js
+++ index.js
@@ -2,20 +2,43 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHTML = require('./utils/generateHTML.js');
+const Manager = require('./lib/Manager.js');
 //storing employee info from prompts
 let employeeArr = [];
-let finalHTML = ``;
+let finalArr;
+let managerArr;
+let engineerArr;
+let internArr;
 const questions = {
     name: `What is the employee's name?`,
     employeeID: `What is the employee's ID?`,
     email: `What is the employee's email?`,
     officeNumber: `What is the office number?`,
     github: `What is the GitHub ID of the engineer?`,
-    school: `What school does the intern attend?`,
-    employeeType: `Would you like to add an engineer, an intern, or finish building your team?`
+    school: `What school does the intern attend?`
 };
 // prompting user for information
 
+const employeeInit = empArr => {
+    managerArr = empArr.filter(empArr => empArr.role === 'Manager');
+    engineerArr = empArr.filter(empArr => empArr.role === 'Engineer');
+    internArr = empArr.filter(empArr => empArr.role === 'Intern');
+
+    managerArr.forEach(manager => {
+        const managerItem = new Manager(manager.name, manager.id, manager.email, manager.officeNumber);
+        finalArr.push(managerItem);
+    });
+    engineerArr.forEach(engineer => {
+        const engineerItem = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github);
+        finalArr.push(engineerItem);
+    });
+    internArr.forEach(intern => {
+        const internItem = new Intern(intern.name, intern.id, intern.email, intern.school);
+        finalArr.push(internItem);
+    });
+
+    console.log(finalArr);
+};
 const promptUser = () => {
     return inquirer.prompt([{
             // This question is only asked when the license question is selected as yes
@@ -25,42 +48,67 @@ const promptUser = () => {
             choices: ['Manager', 'Engineer', 'Intern', 'Finished adding teammates']
         }, {
             type: 'input',
-            name: 'managerName',
-            message: questions.managerName
+            name: 'name',
+            message: questions.name
         },
         {
             type: 'input',
-            name: 'managerID',
+            name: 'id',
             message: questions.employeeID
         },
         {
             type: 'input',
-            name: 'managerEmail',
+            name: 'email',
             message: questions.email
         },
         {
             type: 'input',
             name: 'officeNumber',
-            message: questions.officeNumber
+            message: questions.officeNumber,
+            when: (answers) => answers.role === 'Manager'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: questions.github,
+            when: (answers) => answers.role === 'Engineer'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: questions.school,
+            when: (answers) => answers.role === 'Intern'
         },
 
 
     ]).then((answers) => {
-        console.log('')
+        if (answers.role === 'Finished adding teammates') {
+            employeeArr.push(answers);
+            console.log(answers);
+            console.log(`End of Employee Question`);
+            employeeInit(employeeArr);
+        } else {
+            employeeArr.push(answers);
+            console.log(answers);
+            console.log(`adding more employees`);
+            console.log(employeeArr);
+
+            return promptUser();
+        }
     });
 };
+
+
 // function to initialize app
 const init = () => {
     promptUser()
+        .then(console.log("end of init"))
         // Use writeFileSync method to use promises instead of a callback function
-        .then((answers) => fs.writeFileSync('./output/index.html', generateHTML(answers)))
+        .then(fs.writeFileSync('./dist/index.html', generateHTML(finalArr)))
         .then(() => console.log('Successfully wrote to index.html'))
         .catch((err) => console.error(err));
 };
-// function that renders info
-const render = () => {
 
-}
 
 // Function call to initialize app
 init();
--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -1,54 +1,4 @@
-function generateHTMLStart() {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <link rel="stylesheet" href="styles.css">
-        <title>Nakama Info Generator</title>
-    </head>
-    
-    <body>
-        <header class='banner valign-wrapper '>
-            <h1 class='center-align'>Nakama Info Generator</h1>
-        </header>
-        <div class="container">`;
-}
-
-function generateHTMLEnd() {
-    return `
-    </div>
-</div>
-
-
-<!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-</body>
-
-</html>`;
-}
-
-function generate(employeeType, data) {
-    //checks for employee type
-    switch (employeeType) {
-        case 'Manager':
-            generateManager(data);
-            break;
-        case 'Engineer':
-            generateEngineer(data);
-            break;
-        case 'Intern':
-            generateIntern(data);
-            break;
-        default:
-            'Something went wrong Mr Jones'
-    }
-    //call employee type specifc function
-}
+let teamArr = [];
 
 function generateManager(data) {
     return `<div class="col s12 m4 l3">
@@ -66,6 +16,8 @@ function generateManager(data) {
 </div>`;
 };
 
+//create a for loop/for each to call the generateEngineer/Interns
+
 function generateEngineer(data) {
     return `<div class="col s12 m4 l3">
     <div class="card z-depth-5">
@@ -76,7 +28,7 @@ function generateEngineer(data) {
         <div class="card-content">
             <li>ID: ${data.id}</li>
             <li>Email: mailto: ${data.email}</li>
-            <li>GitHub: ${data.github}</li>
+            <li>GitHub: ${data.getGithub()}</li>
         </div>
     </div>
 </div>`;
@@ -91,11 +43,68 @@ function generateIntern(data) {
         </div>
         <div class="card-content">
             <li>ID: ${data.id}</li>
-            <li>Email: mailto: ${data.email}</li>
-            <li>School: ${data.school}</li>
+            <li>Email: mailto: ${data.getEmail()}</li>
+            <li>School: ${data.getSchool()}</li>
         </div>
     </div>
 </div>`;
 };
 
-module.exports = generateHTMLStart,generateHTMLEnd, generateEngineer, generateIntern, generateManager;
+function generateTeam(teamArr) {
+    //checks for employee type
+    teamArr.forEach(employee => {
+        const role = employee.getRole();
+        switch (role) {
+            case 'Manager':
+                generateManager(employee);
+                break;
+            case 'Engineer':
+                generateEngineer(employee);
+                break;
+            case 'Intern':
+                generateIntern(employee);
+                break;
+            default:
+                'Something went wrong Mr Jones'
+        }
+    })
+
+    //call employee type specifc function
+}
+
+function generateHTML() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="styles.css">
+        <title>Nakama Info Generator</title>
+    </head>
+    
+    <body>
+        <header class='banner valign-wrapper '>
+            <h1 class='center-align'>Nakama Info Generator</h1>
+        </header>
+        <div class="container">
+        ${generateTeam(teamArr)}
+        </div>
+</div>
+
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+
+</html>`;
+}
+
+
+
+
+
+module.exports = generateHTML;
